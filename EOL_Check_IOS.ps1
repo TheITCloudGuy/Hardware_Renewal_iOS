de999013f22aa1a00fe444a980178dbb21933a73
Stephen Devlin <devvbot2@gmail.com>
2023-06-29 20:32:50 +0100

    Working
    Working output collection of data

--- EOL_Check_IOS.ps1
+++ EOL_Check_IOS.ps1
@@ -12,16 +12,34 @@ $iosDevices = Get-IntuneManagedDevice | Get-MSGraphAllPages | Where-Object { $_.
 # Get current date
 $currentDate = Get-Date
 
+# Email parameters
+$sender = "noreply@contoso.com"
+$recipient = "adminuser@contoso.com"
+$subject = "Device Replacement Required"
+$body = "The following iOS devices require replacement:`r`n`r`n"
+
 # Iterate through each iOS device
 foreach ($device in $iosDevices) {
-    $enrollmentDate = $device.enrolledDateTime.DateTime
+    $enrollmentDate = [DateTime]$device.enrolledDateTime
     $age = $currentDate.Year - $enrollmentDate.Year
+    if ($currentDate.Month -lt $enrollmentDate.Month -or ($currentDate.Month -eq $enrollmentDate.Month -and $currentDate.Day -lt $enrollmentDate.Day)) {
+        $age--
+    }
 
     # Check if the device is older than 2 years
     if ($age -ge 2) {
-        Write-Host "Device $($device.deviceName) is older than 2 years. Replacement needed."
-        # Add code here to initiate the replacement process
+        $deviceName = $device.deviceName
+        $enrollmentDateFormatted = $enrollmentDate.ToString("yyyy-MM-dd")
+
+        Write-Host "Device $deviceName is older than 2 years. Replacement needed."
+
+        # Add device details to the email body
+        $body += "Device Name: $deviceName`r`nEnrollment Date: $enrollmentDateFormatted`r`nAge: $age year(s)`r`n`r`n"
     } else {
         Write-Host "Device $($device.deviceName) is within 2 years."
     }
 }
+
+# Send email notification with all device details using SMTP relay
+Send-MailMessage -From $sender -To $recipient -Subject $subject -Body $body -SmtpServer '[SMTP SERVER ADDRESS]' -Port 25
+
